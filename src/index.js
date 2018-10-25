4026e7f830741dad68f341214ee6deb7bd581383
JihyeHwang09 <raye9053@gmail.com>
2018-10-25 14:04:09 +0900

    css 파일 수정

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
-document.querySelector('h1').addEventListener('click', e => {
-  alert('Hello World!');
-});
-const trash = 'me';
+const boardEl = document.querySelector('.board');
+let check = true;
+
+
+document.querySelectorAll('.col').forEach(el => {
+  el.addEventListener('click', e => {
+    console.log(Array.from(el.classList));
+    console.log(Array.from(el.closest('.row').classList));
+
+
+    // 중복된 돌 놓을 수 없다고 알려주기
+    console.log(el.length);
+
+    // 흑돌, 백돌 번갈아 가며 놓기(클래스 추가)
+    // el.classList.add(check ? 'black' : 'white');
+    // check = !check;
+
+
+
+
+  })
+})
